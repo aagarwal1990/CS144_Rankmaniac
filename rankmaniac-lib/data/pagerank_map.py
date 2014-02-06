51eb94f692cae93ac0f7a973a4caf227d2eff955
Anish Agarwal <aagarwal@caltech.edu>
2014-02-05 17:53:33 -0800

    naive pagerank_map coded

--- rankmaniac-lib/data/pagerank_map.py
+++ rankmaniac-lib/data/pagerank_map.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python
 
 import sys
-
+import re
 #
 # This program simply represents the identity function.
 #
 
+adj_dict = {}
+nodes = []
+
+def stringToList(node_str):
+    return re.split('NodeId:|,|\t|\n', node_str)
+    
 for line in sys.stdin:
-    sys.stdout.write(line)
+    line = stringToList(line)
+    nodeID = int(line[1])
+    prev_rank = float(line[2])
+    curr_rank = float(line[3])
+    neighbours = [int(x) for x in line[4:-2]]
+    adj_dict[nodeID] = {'prev_rank': prev_rank, 'curr_rank': curr_rank, \
+                        'neighbours': neighbours}
+    nodes.append(nodeID)
+    
+for i in range(len(nodes)):
+    curr_node = nodes[i]
+    adj_dict[curr_node]['prev_rank']= adj_dict[curr_node]['curr_rank'] 
+    adj_dict[curr_node]['curr_rank'] = float(i)
+    sys.stdout.write(str(curr_node) + ' ' + str(adj_dict[curr_node]) + '\n')
+    
+
+    
+
+    
+
